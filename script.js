075b79ab0075a55bef674824883f94648b78a027
VagyasriPatra <vagyasripatra@gmail.com>
2026-08-16 00:00:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function startLearning() {
+
+    alert(
+        "🎉 Welcome to English Learning Hub!\n\nLet's start learning English."
+    );
+
+}
+
+
+function startQuiz() {
+
+    alert(
+        "🎯 Mock Test is coming next!"
+    );
+
+}
+function openTenses() {
+    document.getElementById("tenses").scrollIntoView({
+        behavior: "smooth"
+    });
+}
